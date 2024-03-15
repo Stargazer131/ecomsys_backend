df5e82db9afdbad172aa0a097926117dac906bf5
HAO <naotomori131@gmail.com>
2024-03-15 16:45:47 +0700

    rename image_search module

--- search/views.py
+++ search/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from product.models import Product
 from product.serializers import ProductSerializer
-from search.image_predictor import predict_similar_image
+from search.image_recommender import predict_similar_image
 
 
 class SearchByNameAPIView(APIView):
